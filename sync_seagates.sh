8373372c614ae13fd20b91ca16b6fdeeb8ae8c36
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2024-11-03 09:58:47 +0200

    Modify seagate script.

--- sync_seagates.sh
+++ sync_seagates.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# Simple but useful script for syncing pictures and LR catalogues between my drives
-rsync -aihW --progress --log-file=ingest.log /Volumes/Seagate\ Expansion\ Drive/Pictures/ /Volumes/Seagate\ 4\ TB/Pictures/
-rsync -aihW --progress --log-file=ingest-lr.log /Users/ksaaskil/Pictures/Lightroom /Volumes/Seagate\ 4\ TB/Lightroom
+# Sync pictures between my drives
+rsync -aihW --progress --log-file=ingest.log ls /Volumes/Expansion/Pictures/ /Volumes/Seagate\ 4\ TB/Pictures/
+# rsync -aihW --progress --log-file=ingest-lr.log /Users/ksaaskil/Pictures/Lightroom /Volumes/Seagate\ 4\ TB/Lightroom
